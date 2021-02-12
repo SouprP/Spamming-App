ffb16a156847386e3b9df47dd11a82c6b97b0475
SouprP <67385974+SouprP@users.noreply.github.com>
2021-02-12 18:45:43 +0100

    Window

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
+canvas = tk.Canvas(root, height=500, width = 600, bg="#263D42")
+canvas.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8)
+root.mainloop()
